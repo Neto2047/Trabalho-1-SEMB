08dd712a99d02149449ab90b47588bafa2178b55
Neto2047 <raimundonetoabc@gmail.com>
2019-11-12 17:01:57 -0300

    ...

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-const unsigned char myImg[120][160] = {114,101,97,116,101,100,32,98,121,32,80,71,77,65,95,73,79,58,58,80,
+const unsigned char myImg[120][160] = 
+{114,101,97,116,101,100,32,98,121,32,80,71,77,65,95,73,79,58,58,80,
 71,77,65,95,87,82,73,84,69,46,10,51,48,48,32,32,50,52,54,10,
 50,53,53,10,52,57,32,32,53,48,32,32,52,56,32,32,52,57,32,32,
 52,56,32,32,52,56,32,32,52,57,32,32,53,48,32,32,53,48,32,32,
@@ -971,64 +972,64 @@ const unsigned char myImg[120][160] = {114,101,97,116,101,100,32,98,121,32,80,71
 // 'C' source line config statements
 
 // CONFIG1L
-#pragma config FEXTOSC = OFF    // External Oscillator Selection (Oscillator not enabled)
-#pragma config RSTOSC = HFINTOSC_64MHZ// Reset Oscillator Selection (HFINTOSC with HFFRQ = 64 MHz and CDIV = 1:1)
+//#pragma config FEXTOSC = OFF    // External Oscillator Selection (Oscillator not enabled)
+//#pragma config RSTOSC = HFINTOSC_64MHZ// Reset Oscillator Selection (HFINTOSC with HFFRQ = 64 MHz and CDIV = 1:1)
 
 // CONFIG1H
-#pragma config CLKOUTEN = OFF   // Clock out Enable bit (CLKOUT function is disabled)
-#pragma config PR1WAY = ON      // PRLOCKED One-Way Set Enable bit (PRLOCK bit can be cleared and set only once)
-#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
-#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
+//#pragma config CLKOUTEN = OFF   // Clock out Enable bit (CLKOUT function is disabled)
+//#pragma config PR1WAY = ON      // PRLOCKED One-Way Set Enable bit (PRLOCK bit can be cleared and set only once)
+//#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+//#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
 
 // CONFIG2L
-#pragma config MCLRE = EXTMCLR  // MCLR Enable bit (If LVP = 0, MCLR pin is MCLR; If LVP = 1, RE3 pin function is MCLR )
-#pragma config PWRTS = PWRT_OFF // Power-up timer selection bits (PWRT is disabled)
-#pragma config MVECEN = ON      // Multi-vector enable bit (Multi-vector enabled, Vector table used for interrupts)
-#pragma config IVT1WAY = ON     // IVTLOCK bit One-way set enable bit (IVTLOCK bit can be cleared and set only once)
-#pragma config LPBOREN = OFF    // Low Power BOR Enable bit (ULPBOR disabled)
-#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled , SBOREN bit is ignored)
+//#pragma config MCLRE = EXTMCLR  // MCLR Enable bit (If LVP = 0, MCLR pin is MCLR; If LVP = 1, RE3 pin function is MCLR )
+//#pragma config PWRTS = PWRT_OFF // Power-up timer selection bits (PWRT is disabled)
+//#pragma config MVECEN = ON      // Multi-vector enable bit (Multi-vector enabled, Vector table used for interrupts)
+//#pragma config IVT1WAY = ON     // IVTLOCK bit One-way set enable bit (IVTLOCK bit can be cleared and set only once)
+//#pragma config LPBOREN = OFF    // Low Power BOR Enable bit (ULPBOR disabled)
+//#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled , SBOREN bit is ignored)
 
 // CONFIG2H
-#pragma config BORV = VBOR_2P45 // Brown-out Reset Voltage Selection bits (Brown-out Reset Voltage (VBOR) set to 2.45V)
-#pragma config ZCD = OFF        // ZCD Disable bit (ZCD disabled. ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
-#pragma config PPS1WAY = ON     // PPSLOCK bit One-Way Set Enable bit (PPSLOCK bit can be cleared and set only once; PPS registers remain locked after one clear/set cycle)
-#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
-#pragma config DEBUG = OFF      // Debugger Enable bit (Background debugger disabled)
-#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Extended Instruction Set and Indexed Addressing Mode disabled)
+//#pragma config BORV = VBOR_2P45 // Brown-out Reset Voltage Selection bits (Brown-out Reset Voltage (VBOR) set to 2.45V)
+//#pragma config ZCD = OFF        // ZCD Disable bit (ZCD disabled. ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
+//#pragma config PPS1WAY = ON     // PPSLOCK bit One-Way Set Enable bit (PPSLOCK bit can be cleared and set only once; PPS registers remain locked after one clear/set cycle)
+//#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+//#pragma config DEBUG = OFF      // Debugger Enable bit (Background debugger disabled)
+//#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Extended Instruction Set and Indexed Addressing Mode disabled)
 
 // CONFIG3L
-#pragma config WDTCPS = WDTCPS_31// WDT Period selection bits (Divider ratio 1:65536; software control of WDTPS)
-#pragma config WDTE = OFF        // WDT Disabled; SWDTEN is ignored
+//#pragma config WDTCPS = WDTCPS_31// WDT Period selection bits (Divider ratio 1:65536; software control of WDTPS)
+//#pragma config WDTE = OFF        // WDT Disabled; SWDTEN is ignored
 
 // CONFIG3H
-#pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
-#pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
+//#pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
+//#pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
 
 // CONFIG4L
-#pragma config BBSIZE = BBSIZE_512// Boot Block Size selection bits (Boot Block size is 512 words)
-#pragma config BBEN = OFF       // Boot Block enable bit (Boot block disabled)
-#pragma config SAFEN = OFF      // Storage Area Flash enable bit (SAF disabled)
-#pragma config WRTAPP = OFF     // Application Block write protection bit (Application Block not write protected)
+//#pragma config BBSIZE = BBSIZE_512// Boot Block Size selection bits (Boot Block size is 512 words)
+//#pragma config BBEN = OFF       // Boot Block enable bit (Boot block disabled)
+//#pragma config SAFEN = OFF      // Storage Area Flash enable bit (SAF disabled)
+//#pragma config WRTAPP = OFF     // Application Block write protection bit (Application Block not write protected)
 
-// CONFIG4H
-#pragma config WRTB = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-30000Bh) not write-protected)
-#pragma config WRTC = OFF       // Boot Block Write Protection bit (Boot Block (000000-0007FFh) not write-protected)
-#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
-#pragma config WRTSAF = OFF     // SAF Write protection bit (SAF not Write Protected)
-#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low voltage programming enabled. MCLR/VPP pin function is MCLR. MCLRE configuration bit is ignored)
+// CONFIG4
+//#pragma config WRTB = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-30000Bh) not write-protected)
+//#pragma config WRTC = OFF       // Boot Block Write Protection bit (Boot Block (000000-0007FFh) not write-protected)
+//#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+//#pragma config WRTSAF = OFF     // SAF Write protection bit (SAF not Write Protected)
+//#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low voltage programming enabled. MCLR/VPP pin function is MCLR. MCLRE configuration bit is ignored)
 
 // CONFIG5L
-#pragma config CP = OFF         // PFM and Data EEPROM Code Protection bit (PFM and Data EEPROM code protection disabled)
+//#pragma config CP = OFF         // PFM and Data EEPROM Code Protection bit (PFM and Data EEPROM code protection disabled)
 
 // CONFIG5H
 
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
-#include <xc.h>
+//#include <xc.h>
 
 // F_OSC
-#define _XTAL_FREQ 64000000
+//#define _XTAL_FREQ 64000000
 
 
 #include <stdio.h>
@@ -1038,7 +1039,7 @@ const unsigned char myImg[120][160] = {114,101,97,116,101,100,32,98,121,32,80,71
 #include <limits.h>
 #include <stdint.h>
 #include <time.h>
-#include "uart.h"
+//#include "uart.h"
 
 void setBit(unsigned char *v, unsigned char i, unsigned char j){
   unsigned short m=0, n=0;
@@ -1387,8 +1388,7 @@ void floodFill (unsigned char binaryMatrix[2400], unsigned char x, unsigned char
 	    resetBit(binaryMatrix,currentX,currentY-1);
             push(currentX, currentY-1);
         }
-    }
-    
+    } 
 }
 
 
@@ -1404,14 +1404,24 @@ void floodFill (unsigned char binaryMatrix[2400], unsigned char x, unsigned char
  * @return
  */
 int runAlgorithm() {
-   
+    int i, h, w;
 
+    for(i=0;i<2400;++i){
+        img[i] = 0;
+    }
 
-    int i;
+    for(h=0;h<120;++h){
+        for(w=0;w<160;++w){
+            if(myImg[h][w] > 125){
+                setBit(img,h,w);
+                printf("tadaaa\n");
+            }
+        }
+    }
     for(i=0;i<2400;++i){
         visited[i]=0;
     }
-   
+
     //erode(img, visited);         // Realizamos uma erosão para limpar pixels "soltos" na imagem.
     //dilate(img, visited);        // Em seguida, uma dilatação, para preservar o tamanho dos elementos.
 
@@ -1424,24 +1434,24 @@ int runAlgorithm() {
 	originalImg[i] = img[i];
     }*/
 
-    for (int h = 0; h < 120; h++) {
-        for (int w = 0; w < 160; w++) {
+    for (h = 0; h < 120; h++) {
+        for (w = 0; w < 160; w++) {
             if ((getBit(img,h,w)==1) && (getBit(visited,h,w)==0)) {    // Se o pixel atual for branco e não tiver sido visitado
                 connectedComps++;                           // aumentamos a contagem de componentes
                 floodFill(img, h, w, visited);       // e preenchemos aquela componente com uma cor qualquer.
             }
         }
     }
-    char numcomps[10];
-    sprintf(numcomps,"%d",connectedComps);
-    UART_Escrever_Texto(numcomps);
+//    char numcomps[10];
+  //  sprintf(numcomps,"%d",connectedComps);
+    //UART_Escrever_Texto(numcomps);
     //printf("\nconnectedComps = %d\n", connectedComps);
-    
+    printf("Numero de componentes: %d\n",connectedComps);
     return 0;
 }
 
 int main() {
-    int i = 0;
+    //int i = 0;
     //while (i<10) {
 	//clock_t start = clock();
     runAlgorithm();
